c9d8d0a3d02c4e491535d68dd3a6993ce4fcfd82
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2024-01-24 22:53:28 +0000

    Restructuring

---
+++ main.py
@@ -0,0 +1,11 @@
+# Importing Important Libraies
+import os
+import csv
+import cv2
+import numpy as np
+import face_recognitione
+from datetime import datetime
+
+
+# Capturing video
+live_capture = cv2.VideoCapture(0)
